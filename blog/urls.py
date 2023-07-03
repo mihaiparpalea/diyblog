5e3d38a5c5c0206f9ca11a3f03d93c5d432859e0
Mihai Parpalea <mihai.parpalea@gmail.com>
2023-07-03 10:56:51 +0300

    bkp pentru testarea proxy-modelului Blogger

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+	path('', views.index, name='index'),
+    path('blog/<int:pk>', views.BlogDetailView.as_view(), name='blog-detail'),
+    path('blogs/', views.BlogListView.as_view(), name='blogs'),
+    path('blogger/<int:pk>', views.BloggerDetailView.as_view(), name='blogger-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Blog, User
+from django.views import generic
+import operator
+import datetime
 
-# Create your views here.
+def index(request):
+
+	# Generate counts of some of the main objects
+	num_blogs = Blog.objects.all().count()
+
+	context = {
+		'num_blogs': num_blogs,
+	}
+
+	# Render the HTML template index.html with the data in the context variable
+	return render(request, 'index.html', context=context)
+
+class BlogListView(generic.ListView):
+	model = Blog
+	ordering = ['-date']
+	paginate_by = 5
+	
+class BlogDetailView(generic.DetailView):
+	model = Blog
+
+class BloggerDetailView(generic.DetailView):
+	model = User
+	users = User.objects
+	blogs = sorted(users.blog_set.all, key=operator.attrgetter('date'))
+
+	context = {
+		'blogs': blogs,
+	}
